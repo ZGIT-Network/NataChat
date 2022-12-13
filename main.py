b48ed79e18cc4215ff98914105da2b158c94d68b
ravizhan <ravizhan@hotmail.com>
2022-12-13 17:50:25 +0000

    增加自定义用户名功能

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ from prompt_toolkit import prompt
 log_time = time.strftime('[%y-%m-%d %H:%M:%S]')
 msg_time = time.strftime('[%Y-%m-%d %H:%M:%S]')
 
-cs = set()
+cs = {}
 
 async def talk(websockets, path):
     try:
         while True:
-
-            if (not websockets in cs):  # 新的访问申请
-                cs.add(websockets)
-                msg = msg_time + 'Hello Welcome to Natachat Server! User' + str(websockets.remote_address)
-                print(log_time + 'New client connected, User IP' + str(websockets.remote_address))
-                logging.info('New client connected, User IP' + str(websockets.remote_address))
+            if (not websockets in cs.keys()):  # 新的访问申请
+                username = path[6:]
+                cs[websockets] = username
+                msg = msg_time + 'Hello Welcome to Natachat Server! User ' + username
+                print(log_time + 'New client connected, User IP' + websockets.remote_address[0] + ' Username '+username)
+                logging.info('New client connected, User IP' + websockets.remote_address[0] + ' Username '+username)
             else:
-                msg = msg_time + ' User ' + str(websockets.remote_address) + 'Say: ' + str(await websockets.recv())
-
+                msg = f"{msg_time} {cs[websockets]} {websockets.remote_address[0]} Say: {str(await websockets.recv())}"
                 print(msg)
-            await asyncio.wait([ws.send(msg) for ws in cs])
+            await asyncio.wait([ws.send(msg) for ws in cs.keys()])
     except Exception as err:
-        cs.remove(websockets)
-        print(log_time + 'A WebSocket Client Disconnected. IP' + str(websockets.remote_address))
-        logging.info('A WebSocket Client Disconnected. IP' + str(websockets.remote_address))
+        logging.error(err)
+        del cs[websockets]
+        print(log_time + 'A WebSocket Client Disconnected. IP' + websockets.remote_address[0])
+        logging.info('A WebSocket Client Disconnected. IP' + websockets.remote_address)
 
 
 print(log_time + 'server running on [localhost:8765]')
@@ -37,6 +37,6 @@ logging.info('server running on [localhost:8765]')
 
 
 
-start_server = websockets.serve(talk, 'localhost', 8765)
+start_server = websockets.serve(talk, '0.0.0.0', 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
