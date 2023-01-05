9aaf0de3a5a6787218047ecc470058fcf8025dbc
云默安 <40316661+Yunmoan@users.noreply.github.com>
2023-01-05 15:28:32 +0800

    新增AES-256-ECB加密通讯

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import time
 import atexit
 import logging
 from prompt_toolkit import prompt
+import random
+import string
+from urllib.parse import unquote
+import split
+import base64
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
+BLOCK_SIZE = 64
 
 
 log_time = time.strftime('[%y-%m-%d %H:%M:%S]')
@@ -12,18 +20,37 @@ msg_time = time.strftime('[%Y-%m-%d %H:%M:%S]')
 
 cs = {}
 
+def add_to_16(value):
+    while len(value) % 16 != 0:
+        value += '\0'
+    return str.encode(value)
+    
 async def talk(websockets, path):
     try:
         while True:
             if (not websockets in cs.keys()):  # 新的访问申请
-                username = path[6:]
+                LocalAESKEY = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
+                userinfo = unquote(str(path[6:]),'utf-8')
+                username = userinfo.split('||')[0]
+                subaeskey = userinfo.split('||')[1]
                 cs[websockets] = username
                 msg = msg_time + 'Hello Welcome to Natachat Server! User ' + username
-                print(log_time + 'New client connected, User IP' + websockets.remote_address[0] + ' Username '+username)
-                logging.info('New client connected, User IP' + websockets.remote_address[0] + ' Username '+username)
+                print(log_time + 'New client connected, User IP' + websockets.remote_address[0] + ' Username '+username+'\nSubmitted AESKEY:'+subaeskey )
+                logging.info('New client connected, User IP' + websockets.remote_address[0] + ' Username '+username+'\nSubmitted AESKEY:'+subaeskey )
+                
+                
             else:
-                msg = f"{msg_time} {cs[websockets]} {websockets.remote_address[0]} Say: {str(await websockets.recv())}"
-                print(msg)
+                message = str(await websockets.recv())
+                # AES-256-ECB
+                key = subaeskey
+                text = message
+                aes = AES.new(add_to_16(key), AES.MODE_ECB)
+                base64_decrypted = base64.decodebytes(text.encode())
+                decrypted_text = str(aes.decrypt(base64_decrypted),encoding='utf-8').replace('\0','') 
+                msg = f"{msg_time} {cs[websockets]} ({websockets.remote_address[0]}) #   { decrypted_text }"
+                print("Not decrypted: "+message)
+                print("Decrypted: "+msg)
+                
             await asyncio.wait([ws.send(msg) for ws in cs.keys()])
     except Exception as err:
         logging.error(err)
